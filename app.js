023cc5d2b6b0bee8631c59bace262e9aaa244f4a
NathSantos <nathsantos2703@gmail.com>
2023-04-09 21:32:30 -0400

    Initial changes on the design

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const https = require('https');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -37,5 +38,5 @@ app.post('/', function(req, res) {
 })
 
 app.listen(port, function() {
-    console.log("Servidor rodando na porta 3000");
+    console.log("Servidor rodando na porta 4000");
 })
